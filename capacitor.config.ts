567ea05e2f268b438ff397c2957a49c6e09d9bc2
joern.e <joern.eggersgluess@gmail.com>
2026-08-19 10:35:14 +0200

    change bundle id, build on ios, change web root path in cap.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'org.leinelab.keyapp_capacitor',
+  appId: 'org.leinelab.keyapp-capacitor',
   appName: 'leinelapp-capacitor',
-  webDir: 'undefined'
+  webDir: 'dist/leinelapp-capacitor/browser'
 };
 
 export default config;
