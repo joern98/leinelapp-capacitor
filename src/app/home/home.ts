1a9183446397e3a59f641514abf80da66a2a3eac
joern.e <joern.eggersgluess@gmail.com>
2026-08-23 14:27:17 +0200

    frontend test for plugion dev implementing shell

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SSH, SSHConnectOptions } from 'capacitor-ssh';
+import ansiRegex from 'ansi-regex';
 
 @Component({
   selector: 'app-home',
@@ -14,11 +15,21 @@ export class Home {
     
     console.log("try ssh");
     const sshConnectOptions: SSHConnectOptions = {
-      host: "",
-      username: "",
+      host: "192.168.145.23",
+      username: "joern-admin",
+      password: "Porto.in.Portugal-2025%",
     }
     const {sessionId} = await SSH.connect({connection: sshConnectOptions});
     console.log(`got session id ${sessionId} from plugin`);
+
+    const {shellId} = await SSH.startShell({sessionId});
+    console.log(`started shell on session ${sessionId} with id ${shellId}`);
+
+    SSH.addListener("shellData", (event) => {
+      console.log(event.data.replaceAll(ansiRegex(), ""));
+    })
+
+
 
   }
 }
