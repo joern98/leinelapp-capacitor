6bfd449fa392237fc92c678c350ea5c1a8748d6a
joern.e <joern.eggersgluess@gmail.com>
2026-08-24 14:06:00 +0200

    frontend for shell backend test

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, linkedSignal, signal } from '@angular/core';
 import { SSH, SSHConnectOptions } from 'capacitor-ssh';
 import ansiRegex from 'ansi-regex';
+import { form, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormField],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+
+  inputModel = signal({
+    text: ""
+  });
+  inputForm = form(this.inputModel);
+
+  private sessionId = ""
+
+  shellOutput = signal("");
+
 
   public async onButtonSSH(event: Event){
     event.preventDefault();
@@ -21,16 +32,21 @@ export class Home {
     }
     const {sessionId} = await SSH.connect({connection: sshConnectOptions});
     console.log(`got session id ${sessionId} from plugin`);
+    this.sessionId = sessionId
+    await SSH.startShell({sessionId});
+    console.log(`started shell on session ${sessionId}`);
 
-    const {shellId} = await SSH.startShell({sessionId});
-    console.log(`started shell on session ${sessionId} with id ${shellId}`);
-
-    SSH.addListener("shellData", (event) => {
-      console.log(event.data.replaceAll(ansiRegex(), ""));
+    SSH.addListener("shellData", (event: any) => {
+      const data = event.data.replaceAll(ansiRegex(), "");
+      console.log(data);
+      this.shellOutput.update((current: string) => current + data + "\n");
     })
+  }
 
+  public async onButtonWriteToShell(event: Event) {
+    event.preventDefault();
 
-
+    await SSH.write({sessionId: this.sessionId, data: this.inputForm.text().value()});
   }
 }
 
